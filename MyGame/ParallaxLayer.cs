68567489a7776354e4f09386483ac8244e81d77f
221 <221@221-00>
2025-12-01 11:16:41 +0300

    фон

---
+++ MyGame/ParallaxLayer.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MyGame
+{
+    public class ParallaxLayer
+    {
+        public Texture2D Texture;
+        public float Factor;
+
+        public ParallaxLayer(Texture2D texture, float factor)
+        {
+            Texture = texture;
+            Factor = factor;
+        }
+
+        public void Draw(SpriteBatch sb, Camera2D camera, int screenW, int screenH)
+        {
+            Vector2 camPos = camera.Position * Factor;
+
+            int w = Texture.Width;
+            int h = Texture.Height;
+
+            float xOffset = camPos.X % w;
+            float yOffset = camPos.Y % h;
+
+            if (xOffset < 0) xOffset += w;
+            if (yOffset < 0) yOffset += h;
+
+            int tilesX = screenW / w + 1;
+            int tilesY = screenH / h + 1;
+
+            for (int x = -1; x <= tilesX; x++)
+            {
+                for(int y = -1; y <= tilesY; y++)
+                {
+                    Vector2 pos = new Vector2(
+                        -xOffset + x * w, 
+                        -yOffset + y * h
+                    );
+
+                    sb.Draw(Texture, pos, Color.White);
+                }
+            }
+        }
+    }
+}
--- MyGame/SpaceGame.cs
+++ MyGame/SpaceGame.cs
@@ -10,33 +10,46 @@ namespace MyGame
         private SpriteBatch _spriteBatch;
 
         private Texture2D _heroTexture;
+        private Texture2D _spaceFarTexture;
+        private Texture2D _spaceMidTexture;
+        private Texture2D _spaceNearTexture;
 
         private Hero _hero;
         private Camera2D _camera;
 
+        ParallaxLayer _layerFar;
+        ParallaxLayer _layerMid;
+        ParallaxLayer _layerNear;
+
+        int screenW, screenH;
+
         public SpaceGame()
         {
             _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
-
-        protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
-
-            base.Initialize();
-        }
-
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            screenW = GraphicsDevice.Viewport.Width;
+            screenH = GraphicsDevice.Viewport.Height;
+
             _heroTexture = Content.Load<Texture2D>("hero");
+            _spaceFarTexture = Content.Load<Texture2D>("space_far");
+            _spaceMidTexture = Content.Load<Texture2D>("space_mid");
+            _spaceNearTexture = Content.Load<Texture2D>("space_near");
 
 
             _hero = new Hero(_heroTexture, new Vector2(100,100));
             _camera = new Camera2D();
+
+            _layerFar = new ParallaxLayer(_spaceFarTexture, 0.1f);
+            _layerMid = new ParallaxLayer(_spaceMidTexture, 0.3f);
+            _layerNear = new ParallaxLayer(_spaceNearTexture, 0.6f);
         }
 
         protected override void Update(GameTime gameTime)
@@ -46,12 +59,27 @@ namespace MyGame
 
             _hero.Update(gameTime, _camera);
 
+            Vector2 targetCameraPos = _hero.Position - new Vector2(screenW/2, screenH/2);
+
+            _camera.Update(
+                Vector2.Lerp(_camera.Position, targetCameraPos, 0.1f)  
+            );
+
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+            _layerFar.Draw(_spriteBatch, _camera, screenW, screenH);
+            _layerMid.Draw(_spriteBatch, _camera, screenW, screenH);
+            _layerNear.Draw(_spriteBatch, _camera, screenW, screenH);
+
+            _spriteBatch.End();
+
 
             _spriteBatch.Begin();
 
